71b5e41eb8dc0d4668983d1f092bfa704d238ca4
Tanvir Ahamed <you@example.com>
2020-04-11 13:59:13 +0600

    update 11/4/2020

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,76 +1,95 @@
-
 (function ($) {
 	"use strict";
-   $(document).ready(function($){
+    
+// tooltip
+    $('[data-toggle="tooltip"]').tooltip()
 
-			 // Sticky Menu
-			$('.js--services-section').waypoint(function(direction){
-			   if(direction == 'down'){
-			         $('nav').addClass('sticky');
-			      } else {
-			         $('nav').removeClass('sticky');
-			   }
-			});
-			// Sticky Menu
+// data - background
+    $("[data-background]").each(function () {
+        $(this).css("background-image", "url(" + $(this).attr("data-background") + ")")
+    });
 
-			// Menu Active Color
-			$('.main-nav li').on('click', function(){
-			   $('.main-nav li').removeClass('active');
-			   $(this).addClass('active');
-			});
-			// End Menu
+// Start Slick Nav
+    $('#mobile-menu').slicknav({
+        prependTo: '.show-mobile-menu',
+        allowParentLinks: true
+    });
+// End Slick Nav
+
+// Sticky Menu
+    $(window).on('scroll', function () {
+        var scroll = $(window).scrollTop();
+        if (scroll < 300) {
+        $(".header-area").removeClass("sticky");
+    } else {
+        $(".header-area").addClass("sticky");
+    }
+  });
+
+// WOW active
+  var wow = new WOW({
+    boxClass:     'wow',      
+    animateClass: 'animated', 
+    offset:       0,         
+    mobile:       true,       
+    live:         true,  
+});
+    wow.init();
+//  End Wow 
+
+// One Page Nav
+    var top_offset = $('.header-area').height() - 10;
+        $('.main-menu').onePageNav({
+        currentClass: 'active',
+        scrollOffset: top_offset,
+    });
 
 
-      // Start Owl-carousel
-      $('.case-study-carousel.style-2').owlCarousel({
-         items: 3,
-         loop: true,
-         dots: true,
-         thumbs: false,
-         nav: true,
-         navText:['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
-         autoplay: true,
-				 autoplayTimeout:1000,
-    		 autoplayHoverPause:true,
-         margin: 30,
-         responsive: {
-            0:{
-                items:1
-            },
-            767:{
-                items: 2
-            },
-            992:{
-                items: 3
-            },
+//  Scroll Up
+    $.scrollUp({
+        scrollName: 'scrollUp',
+        topDistance: '300',
+        topSpeed: 300, 
+        animation: 'fade', 
+        animationInSpeed: 200, 
+        animationOutSpeed: 200, 
+        scrollText: '<i class="fa fa-arrow-up"></i>', 
+        activeOverlay: false, 
+    });
+//   Scroll Up
+
+// Start Owl-carousel
+    $('.case-study-carousel.style-2').owlCarousel({
+        items: 3,
+        loop: true,
+        dots: true,
+        thumbs: false,
+        nav: true,
+        navText:['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
+        autoplay: true,
+                autoplayTimeout:1000,
+            autoplayHoverPause:true,
+        margin: 30,
+        responsive: {
+        0:{
+            items:1
         },
-      });
-      // End Owl-carousel //
+        767:{
+            items: 2
+        },
+        992:{
+            items: 3
+        },
+    },
+    });
+// End Owl-carousel //
 
+// Magnific Popup
+$(".video-play").magnificPopup({
+    type: 'iframe',
+});
+//   End  Magnific Popup
 
-      // Start Isotope //
-      // data filter
-      $('.portfolio-tab button').on('click', function () {
-         var filterData = $(this).attr('data-filter');
-
-         $('.portfolio-list').isotope({
-            filter: filterData,
-         });
-
-      // // Tab Color
-      //    $('.portfolio-tab button').removeClass('active');
-      //    $(this).addClass('active');
-      });
-
-      // initial isotope
-      $('.portfolio-list').isotope({
-         itemSelector: '.single-portfolio-item',
-         percentPosition: true,
-            masonry: {
-               columnWidth: '.single-portfolio-item',
-               horizontalOrder: true,
-            },
-      });
 
       //for menu active class
       $('.portfolio-tab button').on('click', function(event) {
@@ -78,12 +97,22 @@
          $(this).addClass('active');
          event.preventDefault();
       });
-      // End Isotope //
 
+// Smooth Scroll for IE/ EDGE/ SAFARI
+$('a').on('click', function(event){
+    if (this.hash !==''){
+        event.preventDefault();
 
+        var hash = this.hash;
 
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+            }, 800, function(){
+            window.location.hash = hash;
+        });
+    }
+});
+// End Smooth Scroll for IE/ EDGE/ SAFARI
 
-
-   });
 
 })(jQuery);
